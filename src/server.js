6e7a73fc677a52d06e533e6febcab66fee1f3286
O-Kreator <dongjunkang.design@gmail.com>
2021-01-17 22:23:45 +0900

    Chore: reset vercel-sapper related setting

--- src/server.js
+++ src/server.js
@@ -6,10 +6,8 @@ import * as sapper from '@sapper/server'
 const {PORT, NODE_ENV} = process.env
 const dev = NODE_ENV === 'development'
 
-const app = express()
+export default express()
   .use(compression({threshold: 0}), sirv('static', {dev}), sapper.middleware())
   .listen(PORT, (err) => {
     if (err) console.log('error', err)
   })
-
-export default app;
