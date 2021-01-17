e4cc7e2373c5df3a921208d51b2996acbbd2465a
O-Kreator <dongjunkang.design@gmail.com>
2021-01-17 16:31:43 +0900

    Feat: add Bullet common component

---
+++ src/components/commons/Bullet.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let themeChar = 'nyong';
+</script>
+
+<span class={themeChar} />
+
+<style land="scss">
+  span {
+    --bullet-size: 1.25rem;
+    --bullet-border-size: 1.75rem;
+    --bullet-color: var(--color-nyong);
+
+    z-index: -1;
+    display: inline-block;
+    position: relative;
+    top: calc(-0.5em + 5px);
+    left: calc(var(--bullet-size) * -0.5);
+    width: var(--bullet-size);
+    height: var(--bullet-size);
+    margin-right: calc(var(--bullet-size) * -1);
+
+    border-radius: 50%;
+    background: var(--bullet-color);
+
+    &::before {
+      content: '';
+      position: absolute;
+      top: calc(var(--bullet-size) * -0.25);
+      left: calc(var(--bullet-size) * -0.25);
+      width: var(--bullet-border-size);
+      height: var(--bullet-border-size);
+
+      border-radius: 50%;
+      border: 1px solid var(--bullet-color);
+    }
+
+    &.poroo {
+      --bullet-color: var(--color-poroo);
+    }
+  }
+</style>
