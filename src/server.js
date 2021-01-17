0c4943c872291966bce6c88b30397b208bccc792
O-Kreator <dongjunkang.design@gmail.com>
2021-01-17 22:33:22 +0900

    Fix: edit server.js to return app, not void

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ import * as sapper from '@sapper/server'
 const {PORT, NODE_ENV} = process.env
 const dev = NODE_ENV === 'development'
 
-export default express()
-  .use(compression({threshold: 0}), sirv('static', {dev}), sapper.middleware())
-  .listen(PORT, (err) => {
-    if (err) console.log('error', err)
-  })
+const app = express()
+  .use(compression({threshold: 0}), sirv('static', {dev}), sapper.middleware());
+app.listen(PORT, (err) => {if (err) console.log('error', err)});
+
+export default app;
