d767ce78add1d7ab288d5fb08766eabb49402905
O-Kreator <dongjunkang.design@gmail.com>
2021-01-17 16:32:58 +0900

    Feat: add ContentsLeft component in index

---
+++ src/components/index/ContentsLeft.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Bullet from '_components/commons/Bullet.svelte';
+  import Btn from '_components/commons/Btn.svelte';
+
+  import LogoSymbol from '_static/images/logo_symbol.svg';
+  import IconPaperplane from '_static/images/icon_paperplane.svg';
+</script>
+
+<div class="container">
+  <LogoSymbol class="logo-symbol"/>
+  <h1><Bullet />Trust me, I can draw what you want.</h1>
+  <Btn class="index-btn index-btn-commission" onClick={() => {}}>
+    <IconPaperplane />Commission me
+  </Btn>
+  <Btn class="index-btn" onClick={() => {}} styleType="secondary">
+    See info
+  </Btn>
+</div>
+
+<style lang="scss">
+  div.container {
+    text-align: center;
+
+    & > :global(.logo-symbol) {
+      width: 8rem;
+      margin-bottom: var(--space-xs);
+
+      transition-duration: var(--time-short);
+    }
+
+    & > :global(.index-btn) {
+      display: block;
+      margin: var(--space-xs) auto 0;
+      width: 15.625rem;
+      
+      font-size: 1.125rem;
+      text-transform: uppercase;
+    }
+    & > :global(.index-btn-commission) {
+      font-weight: bold;
+    }
+  }
+</style>
