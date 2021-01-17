2f65ce3790860e514433ce293821ef4af4813e7b
O-Kreator <dongjunkang.design@gmail.com>
2021-01-17 18:15:31 +0900

    Feat: add layout of root route

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,64 @@
 <script>
-  import Nav from '../components/Nav.svelte'
-
-  export let segment
 </script>
 
-<Nav {segment} />
-
-<main>
+<main class="border-on">
+  <div class="border" />
+  <div class="border" />
+  <div class="border" />
+  <div class="border" />
   <slot />
 </main>
 
-<style>
+<style lang="scss">
+  @import '../styles/config';
+
   main {
-    position: relative;
-    max-width: 56em;
-    background-color: white;
-    padding: 2em;
-    margin: 0 auto;
-    box-sizing: border-box;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    & > div.border {
+      z-index: 10;
+      position: fixed;
+      background: var(--color-nyong);
+      width: 0;
+      height: 0;
+
+      transition-duration: 0.5s;
+
+      &:nth-child(1) {
+        top: 0;
+        left: 0;
+        width: 100%;
+      }
+      &:nth-child(2) {
+        bottom: 0;
+        left: 0;
+        width: 100%;
+      }
+      &:nth-child(3) {
+        top: 0;
+        left: 0;
+        height: 100%;
+      }
+      &:nth-child(4) {
+        top: 0;
+        right: 0;
+        height: 100%;
+      }
+    }
+
+    &.border-on > div.border {
+      &:nth-child(1),
+      &:nth-child(2) {
+        height: 1.5rem;
+      }
+      &:nth-child(3),
+      &:nth-child(4) {
+        width: 1.5rem;
+      }
+    }
   }
 </style>
