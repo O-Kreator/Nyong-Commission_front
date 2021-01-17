dadd2aa3c318ef15854625917c2652222a553f34
O-Kreator <dongjunkang.design@gmail.com>
2021-01-17 16:32:08 +0900

    Feat: add BackgroundGrid common component

---
+++ src/components/commons/BackgroundGrid.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let width = 'container';
+</script>
+
+<div class={width} />
+
+<style lang="scss">
+  div {
+    &.container {
+      max-width: calc(1366px - 6rem);
+      margin: 0 auto;
+    }
+
+    --grid-color: var(--color-nyong);
+
+    z-index: -1;
+
+    position: fixed;
+    top: 0;
+    left: 3rem;
+    right: 3rem;
+    bottom: 0;
+    background: linear-gradient(90deg, var(--grid-color) 1px, transparent 1px) 0 0;
+    background-size: calc((100% - 1px) / var(--grid-count)) 1px;
+  }
+</style>
