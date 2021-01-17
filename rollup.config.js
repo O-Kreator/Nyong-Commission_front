5968be79aae144c301a7f8896c9143663807a635
O-Kreator <dongjunkang.design@gmail.com>
2021-01-17 19:09:24 +0900

    Chore: fix server output in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -115,7 +115,7 @@ export default {
 
 	server: {
 		input: config.server.input(),
-		output: {...config.server.output(), exports: 'default'},
+		output: config.server.output(),
 		plugins: [
 			replace({
 				'process.browser': false,
