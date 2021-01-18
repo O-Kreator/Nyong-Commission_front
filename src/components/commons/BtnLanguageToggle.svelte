01ac46d1c266bded06f594d949329b486cb24c43
O-Kreator <dongjunkang.design@gmail.com>
2021-01-18 22:17:08 +0900

    Feat: add BtnLanguageToggle common component

---
+++ src/components/commons/BtnLanguageToggle.svelte
@@ -0,0 +1,43 @@
+<script>
+  import {_, locale, locales} from 'svelte-i18n';
+
+  const changeLangToNext = () => {
+    const currentLangIndex = $locales.indexOf($locale);
+    const nextLangIndex = currentLangIndex === $locales.length - 1 ? 0 : currentLangIndex + 1;
+    locale.set($locales[nextLangIndex]);
+  };
+</script>
+
+<button class={`${$$props.class} ${$locale === 'ko-KR' && 'text-en'}`} on:click={changeLangToNext}>
+  {$_(`common.change-lang-to-next`)}
+</button>
+
+<style lang="scss">
+  button {
+    padding: 0;
+
+    border: 0;
+    background: none;
+
+    color: var(--color-text);
+    font-size: 1rem;
+    line-height: 1;
+    text-decoration: underline;
+
+    transform: translateY(0);
+
+    cursor: pointer;
+
+    transition: color var(--time-long), transform var(--time-short);
+
+    &:hover,
+    &:focus {
+      color: var(--color-nyong-bright);
+      transition-duration: var(--time-short);
+    }
+
+    &:active {
+      transform: translateY(2px);
+    }
+  }
+</style>
