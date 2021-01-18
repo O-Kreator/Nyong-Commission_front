974e5149e72fb2e7208e12781c5a45ede517d2a8
O-Kreator <dongjunkang.design@gmail.com>
2021-01-18 14:31:29 +0900

    Feat: add BtnThemeToggle common component

---
+++ src/components/commons/BtnThemeToggle.svelte
@@ -0,0 +1,58 @@
+<script>
+  import {onMount} from 'svelte';
+
+  import IconSun from '_static/images/icon_sun.svg';
+  import IconMoon from '_static/images/icon_moon.svg';
+
+  let isThemeDark = false;
+
+  const setIsThemeDark = () => {
+    return window.document.body.classList.contains('theme_dark');
+  };
+
+  const toggleTheme = () => {
+    window.document.body.classList.toggle('theme_dark');
+    isThemeDark = !isThemeDark;
+  };
+
+  onMount(setIsThemeDark);
+</script>
+
+<button class={$$props.class} on:click={toggleTheme}>
+  {#if isThemeDark}
+    <IconMoon />
+  {:else}
+    <IconSun />
+  {/if}
+</button>
+
+<style>
+  button {
+    padding: 0;
+    width: 24px;
+    height: 24px;
+    line-height: 0;
+
+    border: 0;
+    background: transparent;
+
+    transform: translateY(0);
+    transition: transform var(--time-short);
+
+    cursor: pointer;
+
+    &:active {
+      transform: translateY(2px);
+    }
+
+    & > :global(svg) {
+      fill: var(--color-text);
+      transition: fill var(--time-short);
+    }
+
+    &:hover > :global(svg),
+    &:focus > :global(svg) {
+      fill: var(--color-nyong);
+    }
+  }
+</style>
