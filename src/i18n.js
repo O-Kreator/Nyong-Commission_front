cce9f69758a374ad7e9a26aafe16ee60713d4da0
O-Kreator <dongjunkang.design@gmail.com>
2021-01-18 22:16:09 +0900

    Feat: add init of i18n package
    - Make i18n.js and init in both client and server

---
+++ src/i18n.js
@@ -0,0 +1,45 @@
+import {register, init, getLocaleFromNavigator, locale as $locale} from 'svelte-i18n';
+
+const initOptions = {
+  fallbackLocale: 'en',
+  initialLocale: getLocaleFromNavigator(),
+}
+
+let currentLocale = null;
+
+register('en', () => import('_locales/en.json'));
+register('ko-KR', () => import('_locales/ko.json'));
+
+$locale.subscribe(value => {
+  if (value == null) return;
+
+  currentLocale = value;
+});
+
+// Initializing the i18n library in client.
+export function startClient() {
+  init(initOptions);
+}
+
+// Initializing the i18n library in server and returns its middleware.
+const REGEX_DOCUMENT = /(^([^.?#@]+)?([?#](.+)?)?|service-worker.*?\.html)$/;
+
+export function i18nMiddleware() {
+  init(initOptions);
+
+  return (req, res, next) => {
+    const isDocument = REGEX_DOCUMENT.test(req.originalUrl);
+
+    if (!isDocument) {
+      next();
+      return;
+    }
+
+    const locale = initOptions.initialLocale || initOptions.fallbackLocale;
+    if (locale != null && locale !== currentLocale) {
+      $locale.set(locale);
+    }
+
+    next();
+  };
+}
--- src/server.js
+++ src/server.js
@@ -3,11 +3,18 @@ import express from 'express'
 import compression from 'compression'
 import * as sapper from '@sapper/server'
 
+import { i18nMiddleware } from './i18n.js';
+
 const {PORT, NODE_ENV} = process.env
 const dev = NODE_ENV === 'development'
 
 const app = express()
-  .use(compression({threshold: 0}), sirv('static', {dev}), sapper.middleware());
+  .use(
+    compression({threshold: 0}),
+    sirv('static', {dev}),
+    i18nMiddleware(),
+    sapper.middleware()
+    );
 app.listen(PORT, (err) => {if (err) console.log('error', err)});
 
 export default app;
