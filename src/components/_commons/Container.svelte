40c79175e663a892f3808e1b43f1e027cc2750f1
O-Kreator <dongjunkang.design@gmail.com>
2021-01-20 11:24:47 +0900

    Feat: add container common component

---
+++ src/components/_commons/Container.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let hasMarginTop = false;
+</script>
+
+<div class={`${hasMarginTop ? 'has-margin-top' : ''} ${$$props.class}`}>
+  <slot />
+</div>
+
+<style lang="scss">
+  div {
+    margin: 3rem auto;
+    max-width: calc(1366px - 9rem);
+    width: calc(100% - var(--space-r));
+
+    transition: margin var(--time-long);
+
+    &.has-margin-top {
+      margin-top: calc(var(--height-nav) + 3rem);
+    }
+  }
+</style>
