87a201a8fc5d27b555b3f30f016012acde8efe28
O-Kreator <dongjunkang.design@gmail.com>
2021-01-21 01:27:34 +0900

    Feat: add preloader component

---
+++ src/components/_layout/Preloader.svelte
@@ -0,0 +1,63 @@
+<script>
+  import {cubicInOut} from 'svelte/easing';
+
+  import LogoSymbol from '_static/images/logo_symbol.svg';
+
+  let CONST_DURATION = 500;
+
+  const transitionOut = () => ({
+    duration: CONST_DURATION,
+    delay: 0,
+    easing: cubicInOut,
+    css: (t) => `opacity: ${t}`,
+  });
+
+  export let isLoaded = false;
+</script>
+
+{#if !isLoaded}
+  <div out:transitionOut>
+    <LogoSymbol />
+  </div>
+{/if}
+
+<style lang="scss">
+  @keyframes spinner {
+    from {
+      box-shadow: 0 0 0 0 var(--color-nyong);
+    }
+
+    to {
+      box-shadow: 0 0 0 2rem transparent;
+    }
+  }
+
+  div {
+    z-index: 1000;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background: var(--color-background);
+
+    & > :global(svg) {
+      overflow: visible;
+      width: 8rem;
+      border-radius: 100%;
+
+      animation-name: spinner;
+      animation-duration: var(--time-long);
+      animation-iteration-count: infinite;
+      animation-timing-function: cubic-bezier(0.445, 0.05, 0.55, 0.95);
+    }
+
+    & > :global(svg path) {
+      fill: var(--color-nyong);
+    }
+  }
+</style>
