36f3d4aae112b47473e536417204aa212ebf198b
O-Kreator <dongjunkang.design@gmail.com>
2021-01-22 01:53:34 +0900

    Feat: import btns to footer-center

--- src/components/_layout/FooterCenter.svelte
+++ src/components/_layout/FooterCenter.svelte
@@ -1,8 +1,18 @@
 <script>
+  import FooterBtnIcon from '_components/_layout/FooterBtnIcon.svelte';
   import BtnThemeToggle from '_components/_commons/BtnThemeToggle.svelte';
+
+  import IconTwitter from '_static/images/icon_twitter.svg';
+  import IconGithub from '_static/images/icon_github.svg';
 </script>
 
 <div>
+  <FooterBtnIcon href="https://twitter.com/doro_nyong_art">
+    <IconTwitter />
+  </FooterBtnIcon>
+  <FooterBtnIcon href="https://github.com/O-Kreator/Nyong-Commission_front">
+    <IconGithub />
+  </FooterBtnIcon>
   <BtnThemeToggle />
 </div>
 
@@ -18,12 +28,16 @@
 
     @include media-not-mobile {
       margin-top: 0;
-      margin-bottom: 0;
+      margin-bottom: 0; 
       text-align: right;
     }
 
     @include media-desktop-small {
       text-align: center;
+    }
+
+    & > :global(*:not(:first-child)) {
+      margin-left: var(--space-l);
     }
   }
 </style>
