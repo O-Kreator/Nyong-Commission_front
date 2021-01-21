afb4c595ab118c6589196a8d131a0a05877e07be
O-Kreator <dongjunkang.design@gmail.com>
2021-01-22 01:51:31 +0900

    Feat: add footer-btn-poroo layout component

---
+++ src/components/_layout/FooterBtnPoroo.svelte
@@ -0,0 +1,29 @@
+<a href={$$props.href ? $$props.href : ''}>
+  <img alt="Poroo-mi" src="images/icon_poroo.png" />
+</a>
+
+<style lang="scss">
+  a {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+
+    transform: translateY(0);
+
+    & > img {
+      height: 100%;
+
+      transform: scale(1);
+      transition-duration: var(--time-long);
+    }
+
+    &:hover > img,
+    &:focus > img {
+      transform: scale(2);
+    }
+
+    &:active {
+      transform: translateY(2px);
+    }
+  }
+</style>
