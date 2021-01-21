a5cf3769f3c360b237d7087e9e11efbda066d2e5
O-Kreator <dongjunkang.design@gmail.com>
2021-01-22 00:56:43 +0900

    Fix: edit info page
    - Change title
    - Add styles when in mobile

--- src/components/info/_page.svelte
+++ src/components/info/_page.svelte
@@ -6,15 +6,24 @@
 </script>
 
 <svelte:head>
-  <title>Information - Doro-nyong's Commission</title>
+  <title>Info - Doro-nyong's Commission</title>
 </svelte:head>
 
-<Container hasMarginTop={true}>
+<Container hasMarginTop={true} hasMarginBottom={false}>
   <h1><Bullet />{$_('commons.under-construction')}</h1>
 </Container>
 
-<style>
+<style lang="scss">
+  @import '../../styles/mixin';
+
   h1 {
     text-align: center;
+    margin-bottom: 0;
+
+    transition-duration: var(--time-long);
+
+    @include media-mobile-only {
+      font-size: 1.5rem;
+    }
   }
 </style>
