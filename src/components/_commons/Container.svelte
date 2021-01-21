2387a2fc87caa47ad22b35b783923a9850422c84
O-Kreator <dongjunkang.design@gmail.com>
2021-01-22 00:55:46 +0900

    Feat: add margin-bottom prop to container

--- src/components/_commons/Container.svelte
+++ src/components/_commons/Container.svelte
@@ -1,21 +1,50 @@
 <script>
   export let hasMarginTop = false;
+  export let hasMarginBottom = false;
+
+  const classes = [];
+  if (hasMarginTop) {
+    classes.push('has-margin-top');
+  }
+  if (hasMarginBottom) {
+    classes.push('has-margin-bottom');
+  }
+  if ($$props.class) {
+    classes.push($$props.class);
+  }
 </script>
 
-<div class={`${hasMarginTop ? 'has-margin-top' : ''} ${$$props.class}`}>
+<div class={classes.join(' ')}>
   <slot />
 </div>
 
 <style lang="scss">
-  div {
-    margin: 3rem auto;
-    max-width: calc(1366px - 9rem);
-    width: calc(100% - var(--space-r));
+  @import '../../styles/mixin';
 
-    transition: margin var(--time-long);
+  div {
+    --margin-hor: 3rem;
+
+    margin: var(--margin-hor) auto 0;
+    max-width: calc(1366px - 9rem);
+    width: calc(100% - 2 * var(--space-l));
+
+    transition-property: margin, width;
+    transition-duration: var(--time-long);
+
+    @include media-mobile-only {
+      width: calc(100% - 2 * var(--space-r));
+    }
 
     &.has-margin-top {
-      margin-top: calc(var(--height-nav) + 3rem);
+      margin-top: calc(var(--height-nav) + var(--margin-hor));
+    }
+    
+    &.has-margin-bottom {
+      margin-bottom: calc(var(--margin-hor) - 2rem);
+
+      @include media-mobile-only {
+        margin-bottom: calc(var(--margin-hor) - var(--space-l));
+      }
     }
   }
 </style>
