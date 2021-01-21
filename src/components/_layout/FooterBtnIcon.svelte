39875edd5a8b7d3a0a3210a838ba5998918e310f
O-Kreator <dongjunkang.design@gmail.com>
2021-01-22 01:51:22 +0900

    Feat: add footer-btn-icon layout component

---
+++ src/components/_layout/FooterBtnIcon.svelte
@@ -0,0 +1,31 @@
+<a href={$$props.href ? $$props.href : ''} target="_blank">
+  <slot />
+</a>
+
+<style lang="scss">
+  a {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+
+    transform: translateY(0);
+
+    & > :global(svg) {
+      height: 100%;
+      fill: var(--color-text);
+
+      transition-duration: var(--time-long);
+    }
+
+    &:hover > :global(svg),
+    &:focus > :global(svg) {
+      fill: var(--color-nyong-bright);
+
+      transition-duration: var(--time-short);
+    }
+
+    &:active {
+      transform: translateY(2px);
+    }
+  }
+</style>
