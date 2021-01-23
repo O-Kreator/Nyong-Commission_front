627ee6f95a6790e7686510bebe415ffac1e47382
O-Kreator <dongjunkang.design@gmail.com>
2021-01-23 20:57:50 +0900

    feat: Use localStorage in i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,24 +1,32 @@
 import {register, init, getLocaleFromNavigator, locale as $locale} from 'svelte-i18n';
+import {getLocalStorage, setLocalStorage} from '_services/localStorage.js';
 
 const initOptions = {
   fallbackLocale: 'en',
-  initialLocale: getLocaleFromNavigator(),
+  initialLocale: null,
 };
 
 let currentLocale = null;
 
 register('en', () => import('_locales/en.json'));
-register('ko-KR', () => import('_locales/ko.json'));
+register('ko', () => import('_locales/ko.json'));
 
 $locale.subscribe((value) => {
   if (value == null) return;
 
   currentLocale = value;
+
+  if (typeof window !== 'undefined') {
+    setLocalStorage('locale', value);
+  }
 });
 
 // Initializing the i18n library in client.
 export function startClient() {
-  init(initOptions);
+  init({
+    ...initOptions,
+    initialLocale: getLocalStorage('locale') || getLocaleFromNavigator(),
+  });
 }
 
 // Initializing the i18n library in server and returns its middleware.
@@ -35,7 +43,19 @@ export function i18nMiddleware() {
       return;
     }
 
-    const locale = initOptions.initialLocale || initOptions.fallbackLocale;
+    let locale = getLocalStorage('locale');
+
+    if (typeof locale === 'undefined') {
+      if (req.headers['accept-language']) {
+        const headerLang = req.headers['accept-language'].split(',')[0].trim();
+        if (headerLang.length > 1) {
+          locale = headerLang;
+        }
+      } else {
+        locale = initOptions.initialLocale || initOptions.fallbackLocale;
+      }
+    }
+
     if (locale != null && locale !== currentLocale) {
       $locale.set(locale);
     }
