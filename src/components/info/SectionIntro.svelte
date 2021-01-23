cd30b2d025096f4d07ecaa612232996f868a462f
O-Kreator <dongjunkang.design@gmail.com>
2021-01-24 02:35:31 +0900

    feat: Add section-intro info component

---
+++ src/components/info/SectionIntro.svelte
@@ -0,0 +1,112 @@
+<script>
+  import Bullet from '_components/_commons/Bullet.svelte';
+  import SocialMediaLink from './SocialMediaLink.svelte';
+
+  import IconTwitter from '_static/images/icon_twitter.svg';
+
+  import {_, json} from 'svelte-i18n';
+</script>
+
+<div id="wrapper">
+  <img alt="Profile of Doro-nyong" src="images/profile_doro.jpg" />
+  <div>
+    <h1>
+      <Bullet />{$_('info.introduce.hello')}
+    </h1>
+    <p>
+      {$json('info.introduce.p1')[0]}<br />
+      {$json('info.introduce.p1')[1]}
+    </p>
+    <div id="social-media-links-wrapper">
+      <SocialMediaLink
+        icon={IconTwitter}
+        id={'@doro_nyong_art'}
+        href={'https://twitter.com/doro_nyong_art'}
+      />
+      <SocialMediaLink
+        icon={IconTwitter}
+        id={'Doro-nyong#4031'}
+        href={'https://twitter.com/doro_nyong_art'}
+      />
+    </div>
+    <p>
+      {$json('info.introduce.p2')[0]}<br />
+      {$json('info.introduce.p2')[1]}
+    </p>
+    <p>
+      {$_('commons.under-construction')}
+    </p>
+  </div>
+</div>
+
+<style lang="scss">
+  @import '../../styles/mixin';
+
+  #wrapper {
+    font-size: 1rem;
+
+    @include media-tablet {
+      text-align: center;
+    }
+
+    @include media-desktop-small-only {
+      text-align: center;
+    }
+
+    @include media-desktop-regular {
+      display: flex;
+      flex-direction: row-reverse;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    #social-media-links-wrapper > :global(a:not(:last-child)) {
+      @include media-mobile-only {
+        display: block;
+        margin-bottom: var(--space-s);
+      }
+
+      @include media-not-mobile {
+        margin-right: var(--space-xl);
+      }
+    }
+
+    & > img {
+      width: 10rem;
+      border-radius: 100%;
+
+      transition-duration: var(--time-long);
+    }
+
+    & > div {
+      @include media-tablet {
+        margin-top: var(--space-xl);
+      }
+
+      @include media-desktop-small-only {
+        margin-top: var(--space-xl);
+      }
+
+      & > h1 {
+        font-size: 1.5rem;
+        margin-bottom: var(--space-s);
+
+        transition-duration: var(--time-long);
+
+        & + p {
+          margin-top: var(--space-s);
+        }
+      }
+
+      & > p {
+        margin: var(--space-l) 0;
+
+        transition-duration: var(--time-long);
+
+        &:last-of-type {
+          margin-bottom: 0;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/components/info/SocialMediaLink.svelte
@@ -0,0 +1,41 @@
+<a href={$$props.href} class="text-en">
+  <i><svelte:component this={$$props.icon} /></i><span>{$$props.id}</span>
+</a>
+
+<style lang="scss">
+  a {
+    display: inline-block;
+
+    font-size: initial;
+    line-height: 1;
+
+    transition-property: color, font-size;
+
+    & > i {
+      vertical-align: middle;
+      display: inline-block;
+      width: 24px;
+
+      & > :global(svg) {
+        fill: var(--color-text);
+        transition-duration: var(--time-long);
+      }
+    }
+
+    & > span {
+      margin-left: var(--space-s);
+    }
+
+    &:hover,
+    &:focus {
+      & > i > :global(svg) {
+        fill: var(--color-nyong-bright);
+        transition-duration: var(--time-short);
+      }
+    }
+
+    &:active {
+      transform: translateY(2px);
+    }
+  }
+</style>
